3245424d6ed0d4e6097e2686e69df59233f26367
InfernoDragon0 <mushroomapplepiele@gmail.com>
2020-11-09 21:05:53 +0800

    login page v3 (with register capabilities)

--- login.php
+++ login.php
@@ -13,25 +13,51 @@
 
     <div class="auth">
         <img src="./images/hd/cat1.png"/>
-        <div class="auth-container">
-            <div id="login">
+        <div id="login" class="auth-container active">
                 <br>
                 <br>
                 <p class="auth-header">WEBSITE NAME</p>
-                <input type="text" class="inputs" placeholder="Username"/>
+                <input type="email" class="inputs" placeholder="Email"/>
                 <input type="password" class="inputs" placeholder="Password"/><br>
                 <input type="checkbox" id="cb"/><label for="cb">Remember me</label><br>
                 <br><br>
                 <button class="auth-submit">Login</button><br>
                 <a class="qs" href="./forget.php">Forget Password</a>
                 <hr>
-                <p class="qs">New? Click <a href="#">Here to register</a></p>
+                <p class="qs">New? Click <a onclick="switchTab(event, 'register');">Here to register</a></p>
+        </div>
 
-            </div>
+        <div id="register" class="auth-container">
+                <br>
+                <br>
+                <p class="auth-header">REGISTER NOW</p>
+                <input type="text" class="inputs" placeholder="First Name"/>
+                <input type="text" class="inputs" placeholder="Last Name"/>
+                <input type="email" class="inputs" placeholder="Email Address"/>
+                <input type="password" class="inputs" placeholder="Password"/>
+                <input type="password" class="inputs" placeholder="Re-Password"/><br>
+                <input type="checkbox" id="tc"/><label for="tc">I have read and agreed to the terms and conditions</label><br>
+                <input type="checkbox" id="mailing"/><label for="mailing">I want spam mail</label><br>
+                <br><br>
+                <button class="auth-submit">Register</button><br>
+                <hr>
+                <p class="qs">Have an account? Click <a onclick="switchTab(event, 'login');">Here to login</a></p>
         </div>
 
     </div>
 
+
 </body>
+
+<script>
+    function switchTab(evt, tabName) {
+        var i, tabcontent;
+        tabcontent = document.getElementsByClassName("auth-container");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        document.getElementById(tabName).style.display = "block";
+    }
+</script>
 
 </html>
