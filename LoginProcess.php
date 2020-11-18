ed47c946039eb0ba0c139dae2e1d902ab4bd1bce
weiji <weiji@172.30.137.133>
2020-11-18 20:21:06 +0800

    test loginprocess

--- LoginProcess.php
+++ LoginProcess.php
@@ -1,129 +1,110 @@
-<?php
-$first_name = $last_name = $email = $password_hashed = $errorMsg = "";
-$success = true;
+  <!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 
-// Only process if the form has been submitted via POST.
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   // First Name
-    if (isset($_POST["first_name"])) {
-        $first_name = sanitize_input($_POST["first_name"]);
-    }
-    else {
-        $errorMsg .= "First name is required.<br>";
-        $success = false;
-    }
-    
-    // Last Name
-    if (isset($_POST["last_name"])) {
-        $last_name = sanitize_input($_POST["last_name"]);
-      
-    }
-    else {
-        $errorMsg .= "Last name is required.<br>";
-        $success = false;
-    }
-    
-   // Email Address
-    if (isset($_POST["email"])) {
-        $email = sanitize_input($_POST["email"]);
-        // Additional check to make sure e-mail address is well-formed.
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        
+    <?php
+        include "nav.inc.php";
+    ?>   
+        <?php
+        $email = $first_name = $last_name = $password_hashed = $errorMsg = $errorMsg1 = "";
+        $success = true;
+        if ($_SERVER["REQUEST_METHOD"]=="POST")
         {
-            $errorMsg .= "Invalid email format.";
-            $success = false;
+            //email    
+            if (empty($_POST["email"]))
+            {
+                $errorMsg .= "Email is required.<br>";
+                $success = false;
+            }
+            else
+            {
+                $email = sanitize_input($_POST["email"]);
+                // Additional check to make sure e-mail address is well-formed.
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+                {
+                    $errorMsg .= "Invalid email format.";
+                    $success = false;
+                }  
+            }
+            if  (empty($_POST["password"]))
+            {
+                $errorMsg .= "Password and confirmation are required.";
+                $success=false;
+            }    
+            //input valid, query to DB if email/pwd match
+            if ($success)
+            {
+                authenticateUser();
+            }
+        }
+        else
+        {
+            echo"<h2>This page is not meant to be run directly.</2>";
         }
         
-    }
-    else {
-        $errorMsg .= "Email is required.<br>";
-        $success = false;
-    }
-    
-    // Password
-    if (isset($_POST["password"]) || isset($_POST["password_confirm"])) {
-        // Make sure passwords match
-        if ($_POST["password"] != $_POST["password_confirm"]) {
-            $errorMsg .= "Passwords do not match.<br>";
-            $success = false;
-        }
-        else {
-            $password_hashed = password_hash($_POST["password"], PASSWORD_DEFAULT);
-        }
-    }
-    else 
-    {
-        $errorMsg .= "Password and confirmation are required.<br>";
-        $success = false;
-    }
-    if (!$success) {
-        echo "<p>errors occurred<p><br> " . $errorMsg;
-    }
-    else {
-$res = saveMemberToDB();
-
-    }
-}
-else {
-    echo "<h2>This page is not meant to be run directly.</h2>";
-    echo "<p>You can register at the link below:</p>";
-    echo "<a href='register.php'>Go to Sign Up page...</a>";
-    exit();
-}
-
-function sanitize_input($data) {
- $data = trim($data);
- $data = stripslashes($data);
- $data = htmlspecialchars($data);
- return $data;
-}
-
-function saveMemberToDB() {
-    global $first_name, $last_name, $email, $password_hashed, $errorMsg, $success, $res;
-    // Create database connection.
-        $config = parse_ini_file('../../private/db-config.ini');
-        $conn = new mysqli($config['servername'], $config['username'], $config['password'], 'project1004');
-    // Check connection
-    if ($conn->connect_error)
-    {
-        $errorMsg = "Connection failed: " . $conn->connect_error;
-        $result = 0;
-    }
-    else
-    {
-        $stmt = $conn->prepare("INSERT INTO members (first_name, last_name, email, password) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $first_name, $last_name, $email, $password_hashed);
-        $stmt->execute();
-
-        $result = $conn->insert_id;
-
-        if (!$result)
+        
+        //Helper function that checks input for malicious or unwanted content.
+        function sanitize_input($data)
         {
-            $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-            $result = 0;
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
         }
-
-        $stmt->close();
-    }
-    $conn->close();
-    return $result;
-}
-
-
-if (!$res) {
-    echo "<h2>Oops!</h2>";
-    echo "<h4>The following errors were detected:</h4>";
-    echo "<p>$errorMsg</p>";
-    echo "<a href='register.php'g class='btn btn-danger'>Return to Sign Up</a>";
-}
-else
-{
-    echo "<h2>Your registration is successful!</h2>";
-    echo "<h4>Thank you for signing up, $first_name $last_name</h4>";
-    echo "<a href='login.php' class='btn btn-success'>Log-in</a>";
-    echo "<p>Your account ID is $res</p>";
-}
-
-?>
+        /*
+        * Helper function to authenticate the login.
+        */
+        function authenticateUser()
+        {
+            global $first_name, $last_name, $email, $password_hashed, $errorMsg, $success;
+            // Create database connection.
+            $config = parse_ini_file('../../private/db-config.ini');
+            $conn = new mysqli($config['servername'], $config['username'], $config['password'], 'project1004');
+            // Check connection
+            if ($conn->connect_error)
+            {
+                $errorMsg = "Connection failed: " . $conn->connect_error;
+                $success = false;
+            }
+            else
+            {
+                // Prepare the statement:
+                $stmt = $conn->prepare("SELECT * FROM members WHERE email=?");
+                // Bind & execute the query statement:
+                $stmt->bind_param("s", $email);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                if ($result->num_rows > 0)
+                {
+                    // Note that email field is unique, so should only have
+                    // one row in the result set.
+                    $row = $result->fetch_assoc();
+                    $fname = $row["first_name"];
+                    $lname = $row["last_name"];
+                    $pwd_hashed = $row["password"];
+                    // Check if the password matches:
+                    if (!password_verify($_POST["password"], $pwd_hashed))
+                    {
+                        // Don't be too specific with the error message - hackers don't
+                        // need to know which one they got right or wrong. :)
+                        $errorMsg = "Email not found or password doesn't match...";
+                        $success = false;
+                    }
+                }
+                else
+                {
+                    $errorMsg = "Email not found or password doesn't match...";
+                    $success = false;
+                }
+                $stmt->close();
+                }
+            $conn->close();
+        }
+        ?>
 <?php
 if ($success)
             {
