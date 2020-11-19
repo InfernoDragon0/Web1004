b0e3650b3bb58d58e47cb0535f5c89609ed2211b
InfernoDragon0 <mushroomapplepiele@gmail.com>
2020-11-19 19:52:48 +0800

    fixed car.php mysql

--- car.php
+++ car.php
@@ -4,40 +4,49 @@
     </head>
     <body>
         <?php include "./includes/nav.php" ?>
+        <br>
+        <br>
+        <br>
+    
         <?php
 //do some weird stuff for fetching all cars of the selected brand
         $car = $_GET['id'];
-        echo "your car is " . $car;
+        echo "<br><br><br><p>your car is $car</p>";
 
         helloDb();
 
         function helloDb() {
             global $car;
-            
+
             $config = parse_ini_file('../../private/db-config.ini');
-            $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['project1004']);
+            $conn = new mysqli($config['servername'], $config['username'], $config['password'], "project1004");
 
             if ($conn->connect_error) {
                 $errorMsg = "Connection failed: " . $conn->connect_error;
                 echo $errorMsg;
                 $success = false;
             } else {
-                $stmt = $conn->prepare("SELECT * FROM project1004.car WHERE id=?");
-
-                //int id, varchar catID, varchar brand, varchar heading, float price, int stock, bool forRent, varchar model,text description,varchar bigImage,varchar logo, int isMain
-
-                $stmt->bind_param("s", $car);
+                $stmt = $conn->prepare("SELECT * FROM car WHERE id=?");
+                $stmt->bind_param("i", $car);
                 $stmt->execute();
+                //int id, varchar catID, varchar brand, varchar heading, float price, int stock, bool forRent, varchar model,text description,varchar bigImage,varchar logo, int isMain
+                
                 $result = $stmt->get_result();
+                $row = $result->fetch_assoc(); //not a while loop cos should only take one car 
 
-               
-               if ($success)
-                {
-                    echo $car;
+                if ($result->num_rows > 0) {
+                    echo "<p>" . $row["id"] . "<p>";
+                    echo "<p>" .$row["brand"] . "<p>";
+                    echo "<p>" .$row["model"] . "<p>";
+                    echo "<p>" .$row["price"] . "<p>";
+                    echo "<p>" .$row["stock"] . "<p>";
+
+                    //and more!
                 }
-                else 
-                {
+                else {
+                    echo "No results!";
                 }
+
                 $stmt->close();
             }
         }
