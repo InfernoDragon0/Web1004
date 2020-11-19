9b912d2eb3d604a810cf8b0aefa6b6ee39612bff
TsukiBani <52066898+TsukiBani@users.noreply.github.com>
2020-11-19 16:29:06 +0800

    Update car.php
    car.php

--- car.php
+++ car.php
@@ -4,16 +4,15 @@
     </head>
     <body>
         <?php include "./includes/nav.php" ?>
-
         <?php
 //do some weird stuff for fetching all cars of the selected brand
         $car = $_GET['id'];
-        echo "your brand is " . $car;
+        echo "your car is " . $car;
 
         helloDb();
 
         function helloDb() {
-            global $brand;
+            global $car;
 
             $config = parse_ini_file('../../private/db-config.ini');
             $conn = new mysqli($config['servername'], $config['username'], $config['password'], "project1004");
@@ -24,7 +23,6 @@
                 $success = false;
             } else {
                 $stmt = $conn->prepare("SELECT * FROM car WHERE id=?");
-                echo "succuess";
 
                 //int id, varchar catID, varchar brand, varchar heading, float price, int stock, bool forRent, varchar model,text description,varchar bigImage,varchar logo, int isMain
 
@@ -40,113 +38,49 @@
                     while ($row = $result->fetch_assoc()) {
                         //data for images
                         $bigimage = $row["media"];
-                        $logo = $row["brand"] . ".png"; //
-                        $isMain = $row["isMain"]; //use this to show as the hero image
+                        //$logo = $row["brand"] . ".png"; //
+                        //$isMain = $row["isMain"]; //use this to show as the hero image
                         //data for the car desc
                         $model = $row["model"];
                         $description = $row["description"];
                         $brandx = $row["brand"];
-                        $heading = $row["heading"];
+                        //$heading = $row["heading"];
                         $carid = $row["id"];
                         if ($isMain == 1) {
                             ?>
-                            <div class="brand-main">
-                                <a href="car.php?id="<?php echo $carid ?>>
-                                    <img src="images/hd/<?php echo $bigimage ?>"/>
-                                </a>
-                                <div class="brandhero">
-                                    <p class="hero-title"><?php echo strtoupper($brandx) ?></p>
-                                </div>
+                            <img src="images/hd/<?php echo $bigimage ?>"/>
+                            <div class="brandhero">
+                                <p class="hero-title"><?php echo strtoupper($brandx) ?></p>
                             </div>
-                            <div class="logoheader">
-                                <p class="brand-description"><?php echo $description ?></p>
-                                <img src="images/logos/<?php echo $logo ?>"/>
-                                <p class="short-desc">Cars of <?php echo $brandx ?></p>
-                                <hr class="short">
-                            </div>
+                        </div>
+                        <div class="logoheader">
+                            <p class="brand-description"><?php echo $description ?></p>
+                            <p class="short-desc">Cars of <?php echo $brandx ?></p>
+                            <hr class="short">
+                        </div>
                     <?php
                 }
 
                 if ($d1 == 0) {
                     ?>
-                            <div class="car-container">
-                            <?php
-                            $d1 = 1;
-                        }
-
-                        if ($isMain == 0 && $description != "VIDEO") {
-                            ?>
-                                <div class="brand-car">
-                                    <img src="images/hd/<?php echo $bigimage ?>"/>
-                                    <div class="car-data">
-                                        <p class="car-title"><?php echo $model ?></p>
-                                        <p class="car-description"><?php echo $heading ?></p>
-                                    </div>
-                                </div>
-                    <?php
+                        <div class="car-container">
+                        <?php
+                        $d1 = 1;
+                    }
                 }
 
-                if ($description == "VIDEO") {
-                    $vid = $bigimage;
+                if ($d1 == 1) {
+                    ?>
+                    </div>
+                        <?php
+                        $d1 = 2;
+                    }
+                } else {
+                    echo "<br><br><br><p class='hero-title'>No cars in this brand</p>";
                 }
+                $stmt->close();
             }
-
-            if ($d1 == 1) {
-                ?>
-                        </div>
-                            <?php
-                            $d1 = 2;
-                        }
-
-                        if ($vid != "none") {
-                            ?>
-
-                        <div class="brand-video">
-                            <p class="video-heading"><?php echo strtoupper($brandx) ?> SHOWCASE</p>
-                            <iframe src="<?php echo $vid ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-
-                <?php
-            }
-        } else {
-            echo "<br><br><br><p class='hero-title'>No cars in this brand</p>";
         }
-        $stmt->close();
-    }
-}
-?>
-
-        <script>
-
-            const slider = document.querySelector('.car-container');
-            let isDown = false;
-            let startX;
-            let scrollLeft;
-
-            slider.addEventListener('mousedown', (e) => {
-                isDown = true;
-                //slider.classList.add('active');
-                startX = e.pageX - slider.offsetLeft;
-                scrollLeft = slider.scrollLeft;
-            });
-            slider.addEventListener('mouseleave', () => {
-                isDown = false;
-                //slider.classList.remove('active');
-            });
-            slider.addEventListener('mouseup', () => {
-                isDown = false;
-                //slider.classList.remove('active');
-            });
-            slider.addEventListener('mousemove', (e) => {
-                if (!isDown)
-                    return;
-                e.preventDefault();
-                const x = e.pageX - slider.offsetLeft;
-                const walk = (x - startX) * 3; //scroll-fast
-                slider.scrollLeft = scrollLeft - walk;
-            });
-
-        </script>
-
-    </body>
+        ?>
+</body>
 </html>
