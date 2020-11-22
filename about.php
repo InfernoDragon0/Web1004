3d860395fc6f51f0b7935607ecbaf44c62136732
InfernoDragon0 <mushroomapplepiele@gmail.com>
2020-11-22 13:44:38 +0800

    more bootstrap css and aboutus template

--- about.php
+++ about.php
@@ -1,6 +1,9 @@
 <html>
     <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/contact.css">
+    <link rel="stylesheet" href="./css/features.css">
+    <link rel="stylesheet" href="./css/team.css">
         <?php
             include "./includes/header.php"
         ?>
@@ -10,15 +13,92 @@
         <?php
             include "./includes/nav.php"
         ?>
-<div class="wrapper row2">
-  <div id="container" class="clear">
-    <div id="about-us" class="clear">
-      <section id="about-intro" class="clear">
-        <h2>About Us</h2>
-        <img src="images/demo/960x340.gif" alt="">
-        <p>In sed neque id libero pretium luctus. Vivamus faucibus. Ut vitae elit. In hac habitasse platea dictumst. Proin et nisl ac orci tempus luctus. Aenean lacinia justo at nisi. Vestibulum sed eros sit amet nisl lobortis commodo. Suspendisse nulla. Vivamus ac lorem. Aliquam pulvinar purus at felis. Quisque convallis nulla id ipsum. Praesent vitae urna. Fusce blandit nunc nec mi. Praesent vestibulum hendrerit ante.</p>
-        <p>Vivamus accumsan. Donec molestie pede vel urna. Curabitur eget sem ornare felis gravida vestibulum.Sed pulvinar, tellus in venenatis vehicula, lorem magna dignissim erat, in accumsan ante lorem sit amet lorem.</p>
-      </section>
+      <div class="team-clean">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">The TTeam </h2>
+                <p class="text-center">blah blah blah blah blah blah blah blah blah blah blahnibh erat, pellentesque ut laoreet vitae. </p>
+            </div>
+            <div class="row justify-content-center align-items-center people">
+                <div class="col-md-6 col-lg-2 item"><img class="rounded-circle" src="./images/logos/x1.jpg" width="128" height="128">
+                    <h3 class="name">Cat 1 </h3>
+                    <p class="title">Car Scratcher</p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+                <div class="col-md-6 col-lg-2 item"><img class="rounded-circle" src="./images/logos/x2.png" width="128" height="128">
+                    <h3 class="name">Cat 2</h3>
+                    <p class="title">Seat Warmer</p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+                <div class="col-md-6 col-lg-2 item"><img class="rounded-circle" src="./images/logos/x3.jpg" width="128" height="128">
+                    <h3 class="name">Cat 3</h3>
+                    <p class="title">Windshield Wiper</p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+                <div class="col-md-6 col-lg-2 item"><img class="rounded-circle" src="./images/logos/x4.jpg" width="128" height="128">
+                    <h3 class="name">Cat 4</h3>
+                    <p class="title">Sound Maker</p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+                <div class="col-md-6 col-lg-2 item"><img class="rounded-circle" src="./images/logos/x5.jpg" width="128" height="128">
+                    <h3 class="name">Cat 5</h3>
+                    <p class="title">Engine Checker</p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="features-clean">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Features</h2>
+                <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>
+            </div>
+            <div class="row features">
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-map-marker icon"></i>
+                    <h3 class="name">Works everywhere</h3>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est.</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-clock-o icon"></i>
+                    <h3 class="name">Always available</h3>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est.</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-list-alt icon"></i>
+                    <h3 class="name">Customizable</h3>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est.</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-leaf icon"></i>
+                    <h3 class="name">Organic</h3>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est.</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-plane icon"></i>
+                    <h3 class="name">Fast</h3>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est.</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-phone icon"></i>
+                    <h3 class="name">Mobile-first</h3>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="contact-clean">
+        <form method="post" style="max-width: 700px;">
+            <h2 class="text-center">Contact us</h2>
+            <div class="form-group"><input class="form-control" type="text" name="name" placeholder="Name"></div>
+            <div class="form-group"><input class="form-control is-invalid" type="email" name="email" placeholder="Email"><small class="form-text text-danger">Please enter a correct email address.</small></div>
+            <div class="form-group"><textarea class="form-control" name="message" placeholder="Message" rows="14"></textarea></div>
+            <div class="form-group"><button class="btn btn-primary" type="submit">send </button></div>
+        </form>
+    </div>
+
+    <?php include "./includes/footer.php";?>
 </body>
+
 
 </html>
--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,30 @@
-<footer class="container">
-    <p>Luxauto Pte Ltd Copyright &copy; 2020 | All Rights Reserved</p>
-</footer>
+<div class="footer-dark">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>Cars</h3>
+                    <ul>
+                        <li><a href="./brands.php?brand=bugatti">Bugatti</a></li>
+                        <li><a href="./brands.php?brand=jaguar">Jaguar</a></li>
+                        <li><a href="./brands.php?brand=lamborghini">Lamborgi</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>Others</h3>
+                    <ul>
+                        <li><a href="./login.php">Login</a></li>
+                        <li><a href="./register.php">Register</a></li>
+                        <li><a href="./admin.php">Admin Panel</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 item text">
+                    <h3>Luxauto</h3>
+                    <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                </div>
+                <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+            </div>
+            <p class="copyright"><br>Luxauto&nbsp;Pte&nbsp;Ltd&nbsp;Copyright © 2020&nbsp;|&nbsp;All&nbsp;Rights&nbsp;Reserved<br></p>
+        </div>
+    </footer>
+</div>
