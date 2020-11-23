08f89283b5d4cad298c4efcdb6fa759770f6c6ef
Xu Min <xmin23@gmail.com>
2020-11-24 00:03:45 +0800

    change insertcart.php

--- insertcart.php
+++ insertcart.php
@@ -4,41 +4,71 @@
 $car = $_POST['carid'];
 $qty = $_POST['qty'];
 
-
-helloDb();
-
-function helloDb() {
-    global $car;
-
-    $config = parse_ini_file('../../private/db-config.ini');
-    $conn = new mysqli($config['servername'], $config['username'], $config['password'], "project1004");
     if (isset($_SESSION['memberid'])) {
         $userid = $_SESSION['memberid'];
         $username = $_SESSION['membername'];
         echo $userid;
     }
-
-    if ($conn->connect_error) {
+helloDb();
+function saveCartToDB() {
+    global $car, $qty, $userid;
+    // Create database connection.
+        $config = parse_ini_file('../../private/db-config.ini');
+        $conn = new mysqli($config['servername'], $config['username'], $config['password'], 'project1004');
+    // Check connection
+    if ($conn->connect_error)
+    {
         $errorMsg = "Connection failed: " . $conn->connect_error;
-        echo $errorMsg;
-        $success = false;
-    } else {
-        $stmtinsert = $conn->prepare("INSERT INTO cart(member_id,car_id,qty) VALUES('?','?','?')");
+        $result = 0;
+    }
+    else
+    {
+        $stmt = $conn->prepare("INSERT INTO cart (member_id, car_id, qty) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $userid, $car, $qty);
+        $stmt->execute();
 
-        $stmtinsert->bind_param("iii", $car, $qty, $userid);
-        $stmtinsert->execute();
-        $stmt->bind_param("ssss", $fname, $lname, $email, $pwd_hashed);
-       
-        if (!$stmtinsert->execute()) {
+        $result = $conn->insert_id;
 
+        if (!$result)
+        {
             $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-            $success = false;
-        } else {
-
-            header("Location:cart.php");
+            $result = 0;
         }
+
         $stmt->close();
     }
+    $conn->close();
+    return $result;
 }
+//function helloDb() {
+//    global $car;
+//
+//    $config = parse_ini_file('../../private/db-config.ini');
+//    $conn = new mysqli($config['servername'], $config['username'], $config['password'], "project1004");
+
+//
+//    if ($conn->connect_error) {
+//        $errorMsg = "Connection failed: " . $conn->connect_error;
+//        echo $errorMsg;
+//        $success = false;
+//    } else {
+//        $stmtinsert = $conn->prepare("INSERT INTO cart(member_id,car_id,qty) VALUES('?','?','?')");
+//
+//        $stmtinsert->bind_param("iii", $car, $qty, $userid);
+//        $stmtinsert->execute();
+//       $result = $conn->insert_id;
+//
+//        if (!$result)
+//        {
+//            $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+//            $result = 0;
+//        }
+//         else {
+//
+//            header("Location:cart.php");
+//        }
+//        $stmt->close();
+//    }
+//}
 
 ?>
