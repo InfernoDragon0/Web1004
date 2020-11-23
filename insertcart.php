1a8e9bf5e0680231e596c7becec8bdf240b8fcdd
Xu Min <xmin23@gmail.com>
2020-11-24 00:06:51 +0800

    change insertcart.php

--- insertcart.php
+++ insertcart.php
@@ -4,33 +4,30 @@
 $car = $_POST['carid'];
 $qty = $_POST['qty'];
 
-    if (isset($_SESSION['memberid'])) {
-        $userid = $_SESSION['memberid'];
-        $username = $_SESSION['membername'];
-        echo $userid;
-    }
-helloDb();
+if (isset($_SESSION['memberid'])) {
+    $userid = $_SESSION['memberid'];
+    $username = $_SESSION['membername'];
+    echo $userid;
+}
+saveCartToDB();
+
 function saveCartToDB() {
     global $car, $qty, $userid;
     // Create database connection.
-        $config = parse_ini_file('../../private/db-config.ini');
-        $conn = new mysqli($config['servername'], $config['username'], $config['password'], 'project1004');
+    $config = parse_ini_file('../../private/db-config.ini');
+    $conn = new mysqli($config['servername'], $config['username'], $config['password'], 'project1004');
     // Check connection
-    if ($conn->connect_error)
-    {
+    if ($conn->connect_error) {
         $errorMsg = "Connection failed: " . $conn->connect_error;
         $result = 0;
-    }
-    else
-    {
+    } else {
         $stmt = $conn->prepare("INSERT INTO cart (member_id, car_id, qty) VALUES (?, ?, ?)");
         $stmt->bind_param("sss", $userid, $car, $qty);
         $stmt->execute();
 
         $result = $conn->insert_id;
 
-        if (!$result)
-        {
+        if (!$result) {
             $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
             $result = 0;
         }
@@ -40,12 +37,12 @@ function saveCartToDB() {
     $conn->close();
     return $result;
 }
+
 //function helloDb() {
 //    global $car;
 //
 //    $config = parse_ini_file('../../private/db-config.ini');
 //    $conn = new mysqli($config['servername'], $config['username'], $config['password'], "project1004");
-
 //
 //    if ($conn->connect_error) {
 //        $errorMsg = "Connection failed: " . $conn->connect_error;
@@ -70,5 +67,4 @@ function saveCartToDB() {
 //        $stmt->close();
 //    }
 //}
-
 ?>
