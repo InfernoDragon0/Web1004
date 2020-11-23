d7b1ac17068b9d6cd7defaec7cfd963e77aec9ea
Xu Min <xmin23@gmail.com>
2020-11-23 20:51:08 +0800

    change insertcart.php

--- insertcart.php
+++ insertcart.php
@@ -1,32 +1,41 @@
 
-        <?php
-        $car = $_POST['carid'];
-        $qty = $_POST['qty'];
+<?php
+
+$car = $_POST['carid'];
+$qty = $_POST['qty'];
 
 
-        helloDb();
+helloDb();
 
-        function helloDb() {
-            global $car;
+function helloDb() {
+    global $car;
 
-            $config = parse_ini_file('../../private/db-config.ini');
-            $conn = new mysqli($config['servername'], $config['username'], $config['password'], "project1004");
-            if (isset($_SESSION['memberid'])) {
-                $userid = $_SESSION['memberid'];
-                $username = $_SESSION['membername'];
-            }
+    $config = parse_ini_file('../../private/db-config.ini');
+    $conn = new mysqli($config['servername'], $config['username'], $config['password'], "project1004");
+    if (isset($_SESSION['memberid'])) {
+        $userid = $_SESSION['memberid'];
+        $username = $_SESSION['membername'];
+    }
 
-            if ($conn->connect_error) {
-                $errorMsg = "Connection failed: " . $conn->connect_error;
-                echo $errorMsg;
-                $success = false;
-            } else {
-                $stmtinsert = $conn->prepare("INSERT INTO cart(member_id,car_id,qty) VALUES('?','?','?')");
+    if ($conn->connect_error) {
+        $errorMsg = "Connection failed: " . $conn->connect_error;
+        echo $errorMsg;
+        $success = false;
+    } else {
+        $stmtinsert = $conn->prepare("INSERT INTO cart(member_id,car_id,qty) VALUES('?','?','?')");
 
-                $stmtinsert->bind_param("i,i,i", $car, $qty, $userid);
-                $stmtinsert->execute();
-                header("Location:cart.php");
-                $stmt->close();
-                }
-            }
-            ?>
+        $stmtinsert->bind_param("i,i,i", $car, $qty, $userid);
+        $stmtinsert->execute();
+        if ($stmtinsert >= 1) {
+
+
+            header("Location:cart.php");
+        } else {
+
+            echo $sql;
+        }
+        $stmt->close();
+    }
+}
+
+?>
