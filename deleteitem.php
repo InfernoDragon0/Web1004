ecb6e3374c3fbad1311e67e9d6b89756dda8de55
TsukiBani <52066898+TsukiBani@users.noreply.github.com>
2020-11-23 12:06:53 +0800

    Create deleteitem.php
    create deleteitem.php for cart.php

---
+++ deleteitem.php
@@ -0,0 +1,36 @@
+<?php
+
+$cartidToDelete = $_POST["cartid"];
+$caridToDelete = $_POST["carid"];
+if (isset($_SESSION['memberid'])) {
+    $memberid = $_SESSION['memberid']; //for later use
+} else {
+    //Redirect to login page first
+    //return
+}
+helloDb();
+
+function helloDb() {
+    global $car;
+
+    $config = parse_ini_file('../../private/db-config.ini');
+    $conn = new mysqli($config['servername'], $config['username'], $config['password'], "project1004");
+
+    if ($conn->connect_error) {
+        $errorMsg = "Connection failed: " . $conn->connect_error;
+        echo $errorMsg;
+        $success = false;
+    } else {
+        $stmt = $conn->prepare("DELETE FROM cart WHERE id = ? AND car_id = ? AND member_id = ?");
+        $stmt->bind_param("i,i,i", $cartidToDelete, $caridToDelete,$memberid);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+
+        $stmt->close();
+        header("location:cart.php");
+    }
+}
+
+?>
