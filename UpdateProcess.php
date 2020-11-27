02e7662739fb8ad52f92678122355e623210bb44
weiji <weiji@172.27.51.12>
2020-11-28 00:23:32 +0800

    trying to update db for new user details

--- UpdateProcess.php
+++ UpdateProcess.php
@@ -90,7 +90,7 @@ function UpdateMemberToDB() {
     }
     else
     {  
-        $stmt = $conn->prepare("UPDATE members SET first_name='$first_name' WHERE member_id='$id' ");
+        $stmt = $conn->prepare("UPDATE members SET first_name='$first_name' WHERE member_id='$id'  ");
         
                 $stmt->execute();
                 $result = $stmt->get_result();
