8989bc490fe0d962ea2dfae074a9efb27da4ce27
InfernoDragon0 <mushroomapplepiele@gmail.com>
2020-11-28 17:02:18 +0800

    add actual footer description

--- includes/footer.php
+++ includes/footer.php
@@ -5,22 +5,22 @@
                 <div class="col-sm-6 col-md-3 item">
                     <h3>Cars</h3>
                     <ul>
-                        <li><a href="./brands.php?brand=bugatti">Bugatti</a></li>
-                        <li><a href="./brands.php?brand=jaguar">Jaguar</a></li>
-                        <li><a href="./brands.php?brand=lamborghini">Lamborgi</a></li>
+                        <li><a href="./brand.php?brand=bugatti">Bugatti</a></li>
+                        <li><a href="./brand.php?brand=jaguar">Jaguar</a></li>
+                        <li><a href="./brand.php?brand=lamborghini">Lamborgi</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-6 col-md-3 item">
                     <h3>Others</h3>
                     <ul>
                         <li><a href="./login.php">Login</a></li>
-                        <li><a href="./register.php">Register</a></li>
+                        <li><a href="./404.php">404 Page</a></li>
                         <li><a href="./admin.php">Admin Panel</a></li>
                     </ul>
                 </div>
                 <div class="col-md-6 item text">
                     <h3>Luxauto</h3>
-                    <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                    <p>At LUXAUTO, we believe that everyone should have a chance to enjoy engineering marvels and craftsmanship of rare automobiles from around the world.</p>
                 </div>
                 <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
             </div>
