a94a69bbb26c40ce58d3d7d8f1cb5bdbfa252a8a
InfernoDragon0 <mushroomapplepiele@gmail.com>
2020-11-28 11:09:56 +0800

    orders READ from profile page

--- account.php
+++ account.php
@@ -21,16 +21,81 @@
     <p>Your profile pic here</p>
     <p>reset password?</p>
     <p>email verification?</p>
+    
+    
+    <p> </p>
+    <p> </p>
+    <p> </p>
+    <p> </p>
+    <p> </p>
+    <p> </p>
+ 
+   
     <p>Your order history:</p>
-    
-    <p> </p>
-    <p> </p>
-    <p> </p>
-    <p> </p>
-    <p> </p>
-    <p> </p>
-    
-    <div class="auth">
+    <div class="orderhistory">
+        <?php
+            //order history READ
+            session_start();
+            $memberid = 1; //debug only
+            if (isset($_SESSION['memberid'])) {
+                $memberid = $_SESSION['memberid']; //for later use
+            }
+            else {
+                echo "<meta http-equiv='refresh' content='0;url=./login.php'>";
+                return;
+            }
+
+            $config = parse_ini_file('../../private/db-config.ini');
+            $conn = new mysqli($config['servername'], $config['username'], $config['password'], "project1004");
+
+            if ($conn->connect_error) {
+                $errorMsg = "Connection failed: " . $conn->connect_error;
+                echo $errorMsg;
+                $success = false;
+            } 
+            else {
+
+                //$stmt = $conn->prepare("SELECT * FROM orders os, order_details ods where member_id = ? and os.id = ods.order_id");
+                //select id, chargeid, transaction date and count the number of cars in the order, groupby to allow count()
+                $stmt = $conn->prepare("SELECT os.id, charge_id, transaction_date, saleStatus, count(car_id) cars FROM orders os, order_detail od where member_id = ? and os.id = od.order_id group by os.id");
+                $stmt->bind_param("i", $memberid);
+                $stmt->execute();
+                $result = $stmt->get_result();
+
+                while($row = $result->fetch_assoc()) {
+                    //$carid = $row["car_id"];
+                    //$qty = $row["qty"];
+
+                    //only need to show the order ids
+                    $orderid = $row["id"];
+                    $tdate = $row["transaction_date"];
+                    $chargeid = $row["charge_id"];
+                    $salestatus = $row["saleStatus"];
+                    $totalCars = $row["cars"]; //count(car_id) cars
+
+                    //write html here
+                    ?>
+                        <div class="orderdetails">
+                        <p>ID: <?php echo $orderid?></p>
+                        <p>Transaction Date: <?php echo $tdate?></p>
+                        <p>Charge ID: <?php echo $chargeid?></p>
+                        <p>Status: <?php echo $salestatus?></p>
+                        <p>Number of Cars: <?php echo $totalCars?></p>
+                        </div>
+                        
+
+                    <?php
+
+                }
+            }
+            
+            //order history DELETE
+        ?>
+
+</div>
+
+   
+<div class="auth">
         <img src="./images/hd/cat1.png"/>
         <div id="login" class="auth-container active">
                 <br>
@@ -43,50 +108,6 @@
                 <button class="auth-submit">Login</button><br>
                 </form>
         </div>
-   
-
-<?php
-    //order history READ
-    $memberid = 1; //debug only
-    $orderData = [];
-    if (isset($_SESSION['memberid'])) {
-        $memberid = $_SESSION['memberid']; //for later use
-    }
-    else {
-        //Redirect to login page first
-        //return
-    }
-
-    $config = parse_ini_file('../../private/db-config.ini');
-    $conn = new mysqli($config['servername'], $config['username'], $config['password'], "project1004");
-
-    if ($conn->connect_error) {
-        $errorMsg = "Connection failed: " . $conn->connect_error;
-        echo $errorMsg;
-        $success = false;
-    } 
-    else {
-        $stmt = $conn->prepare("SELECT * FROM orders os, order_details ods where member_id = ? and os.id = ods.order_id");
-        $stmt->bind_param("i", $memberid);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        while($row = $result->fetch_assoc()) {
-            $carid = $row["car_id"];
-            $qty = $row["qty"];
-
-            $orderid = $row["order_id"];
-            $tdate = $row["transaction_date"];
-            $chargeid = $row["charge_id"];
-            $salestatus = $row["saleStatus"];
-
-            //add these to a json array then show on display?
-
-        }
-    }
-    
-    //order history DELETE
-?>
 
 </body>
 
